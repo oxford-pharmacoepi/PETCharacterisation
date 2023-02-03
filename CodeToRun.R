78f0e7cdccf2fb6a96f08ab2a8b1d7cb1d16f72b
AliciaAbellan <98816677+AliciaAbellan@users.noreply.github.com>
2023-02-03 13:17:36 +0100

    initial characterisation

---
+++ CodeToRun.R
@@ -0,0 +1,37 @@
+library(CDMConnector)
+library(dplyr)
+
+# Database connection details -----
+server_dbi<-Sys.getenv("SERVER_DBI")
+port<-Sys.getenv("DB_PORT")
+host<-Sys.getenv("DB_HOST")
+user<-Sys.getenv("DB_USER")
+password<-Sys.getenv("DB_PASSWORD")
+
+db <- dbConnect(RPostgres::Postgres(),
+                dbname = server_dbi,
+                port = port,
+                host = host,
+                user = user,
+                password = password,
+                bigint = "integer")
+
+# The name of the schema that contains the OMOP CDM with patient-level data
+cdm_database_schema<-Sys.getenv("DB_CDM_SCHEMA")
+
+# The name of the schema that contains the vocabularies
+# (often this will be the same as cdm_database_schema)
+vocabulary_database_schema<-cdm_database_schema
+
+# The name of the schema where results tables will be created
+results_database_schema<-Sys.getenv("DB_WRITE_SCHEMA")
+
+# create cdm reference ----
+# cdm <- cdm_From_Con(db, cdm_schema = cdm_schema)
+cdm <- CDMConnector::cdm_from_con(con = db,
+                                  cdm_schema = cdm_database_schema,
+                                  write_schema = results_database_schema)
+cdm$motherTable <- tbl(db, sql("SELECT * FROM omop22t2_cmbd.pregnancy_episode"))
+
+
+
